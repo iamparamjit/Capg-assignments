904658b98b74a415307e72f4a9a34190fbeb06b3
iamparamjit <paramjit.profs@gmail.com>
2022-04-11 20:32:47 +0530

    Add files via upload

---
+++ Core Java/AssignmentsOnString/src/second/joinStrings.java
@@ -0,0 +1,13 @@
+package second;
+
+public class joinStrings {
+
+	public static void main(String[] args) 
+	{
+		String str1="Hello,";
+		String str2="How are you?";
+		System.out.println(str1+" " +str2);
+		
+	}
+
+}
