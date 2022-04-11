eea5387659204b81ccc585539ece41a95edc6aa3
iamparamjit <paramjit.profs@gmail.com>
2022-04-11 20:34:27 +0530

    Add files via upload

---
+++ Core Java/ExceptionalHandling/src/second/exceptionalHandling2.java
@@ -0,0 +1,35 @@
+package second;
+
+
+
+import java.util.Scanner;
+public class exceptionalHandling2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter dividend:");
+		int dividend = sc.nextInt();
+		
+		System.out.println("Enter divisor:");
+		int divisor = sc.nextInt();
+		
+		try
+		{
+			if(divisor==0)
+			{
+				throw new UnsupportedOperationException("Divisor can't be 0.");
+			}
+			System.out.println("quotient:"+(dividend/divisor));
+		}
+		catch (UnsupportedOperationException uoe)
+		{
+			System.out.println(uoe.getMessage());
+		}
+		finally
+		{
+			sc.close();
+		}
+
+	}
+
+}
